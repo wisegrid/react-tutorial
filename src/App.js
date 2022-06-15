d1d20f527732580af6429c421c2fe9790b3cbfcc
BongSoo-Shin <shine1024@unipost.co.kr>
2022-06-15 18:26:25 +0900

    first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        data: []
+    };
+
+    componentDidMount() {
+        const url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=pizza&format=json&origin=*';
+        fetch(url)
+            .then((result) => {
+                return result.json();
+            })
+            .then((result) => {
+                this.setState({
+                    data: result
+                });
+            });
+    }
+
+    render() {
+        const {data} = this.state;
+        const result = data.map((entry, index) => {
+            return <li key={index}>{entry}</li>;
+        });
+        return <ul>{result}</ul>;
+    }
 }
 
 export default App;
